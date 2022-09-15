831602674c4c469bbf86d96e4f3fbcfa3b15c1a2
akksinya <akksin.ya999@gmail.com>
2022-09-15 22:37:47 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('.login-form');
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+
+        window.alert("Please fill in all the fields!");
+    };
+
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+    event.currentTarget.reset();
+
+};
+
+formRef.addEventListener("submit", handleSubmit);
+
 
