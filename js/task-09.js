d571d9b3dceca907832811cea2d50a3f0d4c1d3d
akksinya <akksin.ya999@gmail.com>
2022-09-16 22:07:09 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector('body');
+
+console.log(bodyRef);
+
+const colorTextRef = document.querySelector('.color');
+
+console.log(colorTextRef);
+
+const buttonRef = document.querySelector('.change-color');
+
+console.log(buttonRef);
+
+const ChangeBodyColor = () => {
+
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorTextRef.textContent = getRandomHexColor();
+};
+
+buttonRef.addEventListener('click', ChangeBodyColor);
